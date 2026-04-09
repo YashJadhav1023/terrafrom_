f9422c3a5661b2d7c99d529378c4bbd91234a691
YashJadhav1023 <jadhavpc0707@gmail.com>
2026-04-09 05:17:41 +0000

    myawspro

---
+++ var.tf
@@ -0,0 +1,21 @@
+variable "AWS_REGION" {
+    default = "ap-south-1"
+}
+variable "AWS_AMI" {
+    default = "ami-048f4445314bcaa09"
+}
+variable "az" {
+    default = "ap-south-1a"
+}
+variable "akey" {
+    default = ""
+}
+variable "skey" {
+    default = ""
+}
+variable "pkey" {
+    default = "Mumbai-access-key"
+}
+variable "rpass" {
+    default = "111"
+}
